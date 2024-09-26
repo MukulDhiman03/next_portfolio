08806687444640d487158d76ed202b724aefe9fc
MukulDhiman03 <dhimanmukul307@gmail.com>
2024-09-26 15:18:49 +0530

    Footer Change

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,51 +1,58 @@
 import React from 'react'
 import { AnimatedTooltip } from '../ui/animated-tooltip'
 import img from "../../images/react.png"
+import gmail from "../../images/social media/gmail.png"
+import github from "../../images/social media/github.jpg"
+import linkedin from "../../images/social media/linkedin.png"
+import instagram from "../../images/social media/instagram.jpg"
+import youtube from "../../images/social media/youtube.jpg"
 
 const socialHandles = [
     {
         id: 1,
-        name: "GitHub",
-        designation: "Software Engineer",
-        image: img
+        name: "Gmail",
+        designation: "dhimanmukul307@gmail.com",
+        image: gmail,
+        src: "https://mail.google.com", // Add the correct Gmail URL
     },
     {
         id: 2,
-        name: "LinkedIn",
-        designation: "Software Engineer",
-        image: img
+        name: "GitHub",
+        designation: "MukulDhiman03",
+        image: github,
+        src: "https://github.com/MukulDhiman03", // Add the correct GitHub URL
     },
     {
         id: 3,
-        name: "Instagram",
-        designation: "Software Engineer",
-        image: img
+        name: "LinkedIn",
+        designation: "mukuldhiman03",
+        image: linkedin,
+        src: "https://www.linkedin.com/in/mukuldhiman03/", // Add the correct LinkedIn URL
     },
     {
         id: 4,
-        name: "Youtube",
-        designation: "Software Engineer",
-        image: img
+        name: "Instagram",
+        designation: "mukul_dhiman_03",
+        image: instagram,
+        src: "https://www.instagram.com/mukul_dhiman_03/", // Add the correct Instagram URL
     },
-]
+    {
+        id: 5,
+        name: "YouTube",
+        designation: "CodeSadhak",
+        image: youtube,
+        src: "https://www.youtube.com/@CodeSadhak", // Add the correct YouTube URL
+    },
+];
 
 const Footer = () => {
     return (
         <div className='flex flex-col items-center'>
-            <div className="bg-gradient-to-r bg-gray-700 text-white p-6 rounded-lg shadow-lg text-center space-y-4 my-8">
-                <div className="text-xl sm:text-2xl md:text-3xl font-semibold">
-                    Reach out to me today and let's discuss how I can help you achieve your goals.
-                </div>
-                <div className="text-2xl sm:text-3xl md:text-4xl font-bold uppercase tracking-wider">
-                    Let's Get In Touch
-                </div>
-            </div>
-
             <div className="flex flex-row items-center justify-center w-full my-6">
                 <AnimatedTooltip items={socialHandles} />
             </div>
 
-            <div className='text-1xl' >Copyright © 2024 Mukul Dhiman | All Rights Reserved
+            <div className='text-2xl font-bold' >Copyright © 2024 Mukul Dhiman
             </div>
         </div>
     )
